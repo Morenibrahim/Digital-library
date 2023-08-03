b8051bae99122b4f7fab5330a19bb19d6861ce63
Morenibrahim <140696511+Morenibrahim@users.noreply.github.com>
2023-08-03 16:10:16 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,9 @@
 class Book:
-    def __init__(self, title, author, year):
+    def __init__(self, title, author, year, category):
         self.title = title
         self.author = author
         self.year = year
+        self.category = category
 
     def __str__(self):
         return f"{self.title} by {self.author} ({self.year})"
@@ -45,9 +46,9 @@ class Library:
 def main():
     library = Library()
 
-    book1 = Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 1997)
-    book2 = Book("To Kill a Mockingbird", "Harper Lee", 1960)
-    book3 = Book("1984", "George Orwell", 1949)
+    book1 = Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 1997, "mystery")
+    book2 = Book("To Kill a Mockingbird", "Harper Lee", 1960, "action")
+    book3 = Book("1984", "George Orwell", 1949, "history")
 
     library.add_book(book1)
     library.add_book(book2)
